f268950e3a4be5e63a0ff2ff50a06896731940bd
Anton Shaposhnikov (Z003ZBJN) <anton.shaposhnikov@siemens.com>
2021-08-23 21:50:27 +1000

    add reference class, import ref csv

--- src/main.py
+++ src/main.py
@@ -1,4 +1,5 @@
 import csv
+from os import read
 from pathlib import Path
 from dataclasses import dataclass, field
 
@@ -19,9 +20,18 @@ class Table:
     def add_column(self, column: Column) -> None:
         return self.columns.append(column)
 
+@dataclass
+class Reference:
+    """Class represents reference between two tables"""
+    table_name: str = None
+    column_name : str = None
+    referenced_table_name: str = None
+    referenced_column_name: str = None
+
 class Dot:
     def __init__(self) -> None:
-        self.tables = {}
+        self.tables: dict[str, Table] = {}
+        self.references: dict[str, Reference] = {}
 
     def __getitem__(self, table_name: str) -> Table:
         return self.tables[table_name]
@@ -29,21 +39,51 @@ class Dot:
     def add_table(self, table_name: str, table: Table) -> None:
         self.tables[table_name] = table
 
-def read_data_from_csv(csv_path: Path) -> None:
+    def add_reference(self, ref_name: str, reference: Reference) -> None:
+        self.references[ref_name] = reference
+
+def read_data_from_csv(tables_csv_path: Path, references_csv_path: Path) -> None:
 
     dot = Dot()
 
-    with open(csv_path, mode='r') as csvfile:
-        reader = csv.DictReader(csvfile, delimiter=';')
+    with open(tables_csv_path) as csvfile:
+        reader = csv.DictReader(f=csvfile, delimiter=';')
         for row in reader:
             table_name = '.'.join([row['TABLE_SCHEMA'], row['TABLE_NAME']])
 
             if table_name not in dot.tables:
                 dot.add_table(table_name, Table(name=table_name))
 
-            dot[table_name].add_column(Column(name=row['COLUMN_NAME'], data_type=row['DATA_TYPE'], nullable=row['IS_NULLABLE']))
+            dot[table_name].add_column(
+                Column(
+                    name=row['COLUMN_NAME'], 
+                    data_type=row['DATA_TYPE'], 
+                    nullable=row['IS_NULLABLE']
+                    )
+                )
+
+    with open(references_csv_path) as csvfile:
+        reader = csv.DictReader(f=csvfile, delimiter=';')
+        for row in reader:
+            table_name = '.'.join([row['FK schema'], row['FK table']])
+            table_column = row['FK column']
+            ref_table_name = '.'.join([row['Referenced schema'], row['Referenced table']])
+            ref_table_column = row['Referenced column']
+
+            ref_name = f"{'.'.join([table_name, table_column])}->{'.'.join([ref_table_name, ref_table_column])}"
+            dot.add_reference(
+                ref_name, 
+                Reference(
+                    table_name=table_name, 
+                    column_name=table_column, 
+                    referenced_table_name=ref_table_name, 
+                    referenced_column_name=ref_table_column
+                    )
+                )
+
 
     print(dot.tables['dlfe.TasksUpdate'])
+    print(dot.references['dlfe.LeaseContracts.ProjectID->dlfe.Projects.ProjectID'])
 
 if __name__ == '__main__':
-    read_data_from_csv(Path('ddl.csv'))
+    read_data_from_csv(Path('ddl.csv'), Path('references.csv'))
