2463d9b132e991240a067cc09995427289890238
Anton Shaposhnikov (Z003ZBJN) <anton.shaposhnikov@siemens.com>
2021-08-24 17:49:09 +1000

    add schema class

--- src/main.py
+++ src/main.py
@@ -15,11 +15,21 @@ class Column:
 @dataclass
 class Table:
     """Class represents table in database."""
-    table_name: str
-    columns: list[Column] = field(default_factory=list)
+    name: str
+    fullname: str
+    columns: dict[str, Column] = field(default_factory=dict)
 
-    def add_column(self, column: Column) -> None:
-        return self.columns.append(column)
+    def add_column(self, column_name: str, column: Column) -> None:
+        self.columns[column_name] = column
+
+@dataclass
+class Schema:
+    """Class represents schema in database."""
+    name: str
+    tables: dict[str, Table] = field(default_factory=dict)
+
+    def add_table(self, table_name: str, table: Table) -> None:
+        self.tables[table_name] = table
 
 @dataclass
 class Reference:
@@ -31,41 +41,46 @@ class Reference:
 
 class Dot:
 
-    HTML_TABLE_TEMPLATE = '<<table>{thead}{tbody}</table>>'
-    HTML_TABLE_HEAD_TEMPLATE = '<tr><td colspan="2">{thead}</td></tr>'
-    HTML_TABLE_BODY_TEMPLATE = '<tbody>{tbody}</tbody>'
-    HTML_TABLE_ROW_TEMPLATE = '<tr><td port="{port}">{name}</td><td>{datatype}</td></tr>'
-    
+    __HTML_TABLE_TEMPLATE = '<<table>{thead}{tbody}</table>>'
+    __HTML_TABLE_HEAD_TEMPLATE = '<tr><td colspan="2">{thead}</td></tr>'
+    __HTML_TABLE_BODY_TEMPLATE = '<tbody>{tbody}</tbody>'
+    __HTML_TABLE_ROW_TEMPLATE = '<tr><td port="{port}">{name}</td><td>{datatype}</td></tr>'
+
     def __init__(self) -> None:
-        self.tables: dict[str, Table] = {}
+        self.schemas: dict[str, Schema] = {}
         self.references: dict[str, Reference] = {}
 
-    def __getitem__(self, table_name: str) -> Table:
-        return self.tables[table_name]
+    # def __getitem__(self, schema_name: str) -> Table:
+    #     return self.schemas[schema_name]
 
-    def add_table(self, table_name: str, table: Table) -> None:
-        self.tables[table_name] = table
+    def add_schema(self, schema_name: str, schema: Schema) -> None:
+        self.schemas[schema_name] = schema
+
+    # def add_table_to_schema(self, schema_name: str, table_name: str, table: Table) -> None:
+    #     self.schemas[schema_name].tables[table_name] = table
 
     def add_reference(self, ref_name: str, reference: Reference) -> None:
         self.references[ref_name] = reference
 
     def __get_html_table(self, table: Table) -> str:
-        thead = self.HTML_TABLE_HEAD_TEMPLATE.format(thead=table.table_name)
+        thead = self.__HTML_TABLE_HEAD_TEMPLATE.format(thead=table.fullname)
         tbody = ''
-        for column in table.columns:
-            tbody += self.HTML_TABLE_ROW_TEMPLATE.format(port=column.name, name=column.name, datatype=column.data_type)
+        for column in table.columns.values():
+            tbody += self.__HTML_TABLE_ROW_TEMPLATE.format(port=column.name, name=column.name, datatype=column.data_type)
         
-        return self.HTML_TABLE_TEMPLATE.format(thead=thead, tbody=tbody)
-        
+        tbody = self.__HTML_TABLE_BODY_TEMPLATE.format(tbody=tbody)
+        return self.__HTML_TABLE_TEMPLATE.format(thead=thead, tbody=tbody)
+
     def get_dot(self, **kwargs) -> Digraph:
         dot = Digraph(**kwargs)
 
-        for name, table in self.tables.items():
-            dot.node(name, label=self.__get_html_table(table))
+        for schema in self.schemas.values():
+            for table in schema.tables.values():
+                dot.node(table.fullname, label=self.__get_html_table(table=table))
 
         for ref in self.references.values():
             dot.edge(
-                ':'.join([ref.table_name, ref.column_name]), 
+                ':'.join([ref.table_name, ref.column_name]),
                 ':'.join([ref.referenced_table_name, ref.referenced_column_name])
                 )
 
@@ -78,15 +93,28 @@ def read_data_from_csv(tables_csv_path: Path, references_csv_path: Path) -> None
     with open(tables_csv_path) as csvfile:
         reader = csv.DictReader(f=csvfile, delimiter=';')
         for row in reader:
-            table_name = '.'.join([row['schema'], row['table']])
+            schema_name = row['schema']
+            table_name = row['table']
+            column_name = row['column']
+            column_type = row['column_type']
 
-            if table_name not in dot.tables:
-                dot.add_table(table_name, Table(table_name=table_name))
+            if schema_name not in dot.schemas:
+                dot.add_schema(schema_name, Schema(name=schema_name))
 
-            dot[table_name].add_column(
-                Column(
-                    name=row['column'], 
-                    data_type=row['column_type'], 
+            if table_name not in dot.schemas[schema_name].tables:
+                dot.schemas[schema_name].add_table(
+                    table_name=table_name, 
+                    table=Table(
+                        name=table_name, 
+                        fullname='.'.join([schema_name, table_name])
+                        )
+                    )
+
+            dot.schemas[schema_name].tables[table_name].add_column(
+                column_name=column_name,
+                column=Column(
+                    name=column_name,
+                    data_type=column_type,
                     nullable=''
                     )
                 )
@@ -101,20 +129,20 @@ def read_data_from_csv(tables_csv_path: Path, references_csv_path: Path) -> None
 
             ref_name = f"{'.'.join([table_name, table_column])}->{'.'.join([ref_table_name, ref_table_column])}"
             dot.add_reference(
-                ref_name, 
+                ref_name,
                 Reference(
-                    table_name=table_name, 
-                    column_name=table_column, 
-                    referenced_table_name=ref_table_name, 
+                    table_name=table_name,
+                    column_name=table_column,
+                    referenced_table_name=ref_table_name,
                     referenced_column_name=ref_table_column
                     )
                 )
 
     for engine in ['dot', 'neato', 'twopi', 'fdp', 'osage', 'patchwork', 'sfdp']:
         print(engine)
-        dot.get_dot(filename=f'{engine}_ortho_false', engine=engine, format='svg', graph_attr={'splines': 'ortho', 'overlap': 'false'}, node_attr={'shape': 'plaintext'}).render(view=True)
-        dot.get_dot(filename=f'{engine}_ortho_prism', engine=engine, format='svg', graph_attr={'splines': 'ortho', 'overlap': 'prism'}, node_attr={'shape': 'plaintext'}).render(view=True)
-        dot.get_dot(filename=f'{engine}_ortho_true', engine=engine, format='svg', graph_attr={'splines': 'ortho', 'overlap': 'true'}, node_attr={'shape': 'plaintext'}).render(view=True)
+        #dot.get_dot(filename=f'{engine}_ortho_false', engine=engine, format='svg', graph_attr={'splines': 'ortho', 'overlap': 'false'}, node_attr={'shape': 'plaintext'}).render(view=True)
+        #dot.get_dot(filename=f'{engine}_ortho_prism', engine=engine, format='svg', graph_attr={'splines': 'ortho', 'overlap': 'prism'}, node_attr={'shape': 'plaintext'}).render(view=True)
+        #dot.get_dot(filename=f'{engine}_ortho_true', engine=engine, format='svg', graph_attr={'splines': 'ortho', 'overlap': 'true'}, node_attr={'shape': 'plaintext'}).render(view=True)
         dot.get_dot(filename=f'{engine}', engine=engine, format='svg', node_attr={'shape': 'plaintext'}).render(view=False)
 
     return None
