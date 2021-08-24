d6df8ce7a1f8908a5423ad1ad1cfc04f95194de7
Anton Shaposhnikov (Z003ZBJN) <anton.shaposhnikov@siemens.com>
2021-08-24 18:23:53 +1000

    fix referenced tables in ddl query

--- sql/ddl.sql
+++ sql/ddl.sql
@@ -9,12 +9,16 @@ FROM sys.schemas AS schemas
 JOIN sys.tables AS tables ON schemas.schema_id = tables.schema_id
 JOIN sys.columns AS columns ON tables.object_id = columns.object_id
 JOIN sys.types AS column_types ON columns.user_type_id = column_types.user_type_id
-WHERE 
-    schemas.schema_id = @SCHEMA_ID 
+WHERE
+    schemas.schema_id = @SCHEMA_ID
     OR EXISTS (
         SELECT *
         FROM sys.foreign_key_columns AS fk_columns
-        WHERE tables.object_id IN (fk_columns.parent_object_id, fk_columns.referenced_object_id)
+        JOIN sys.tables AS parent_tables ON fk_columns.parent_object_id = parent_tables.object_id
+        JOIN sys.tables AS referenced_tables ON fk_columns.referenced_object_id = referenced_tables.object_id
+        WHERE
+            tables.object_id IN (fk_columns.parent_object_id, fk_columns.referenced_object_id)
+            AND @SCHEMA_ID IN (parent_tables.schema_id, referenced_tables.schema_id)
     )
 ORDER BY
     schemas.name,
