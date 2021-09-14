f084efff9ac13e610abef392fead7e42d4a98499
Anton Shaposhnikov <coolerprog+github@gmail.com>
2021-09-15 01:04:50 +1000

    add ajax for select tables

--- erd_viewer/app.py
+++ erd_viewer/app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request, jsonify
 from erd_viewer.loader.redis import RedisClient
 
 app = Flask(__name__)
@@ -6,11 +6,18 @@ app = Flask(__name__)
 @app.route("/", methods=['GET', 'POST'])
 def index():
 
-    redis = RedisClient().r
+    redis = RedisClient().get_client()
     schemas = sorted(redis.keys('*'), key=str.casefold)
     tables = sorted(redis.hkeys('dlfe'), key=str.casefold)
-
     return render_template('graph.html', schemas=schemas, tables=tables)
+
+@app.route("/get_tables", methods=['POST'])
+def get_tables():
+
+    redis = RedisClient().get_client()
+    schema = request.form.get('schema')
+    tables = sorted(redis.hkeys(schema), key=str.casefold)
+    return jsonify(tables)
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=4444, debug=True)
